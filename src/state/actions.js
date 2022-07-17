ab482869d42a2a9201b6769f65b4216d2da189df
Nick Garbalau <garbalaunick@protonmail.com>
2022-07-17 19:51:24 +0300

    Error is gone, the problem not, need Async Middleware

--- src/state/actions.js
+++ src/state/actions.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
-export const fetchPosts = async () => {
-  const response = await axios.get(
-    'https://jsonplaceholder.typicode.com/posts'
-  );
+export const fetchPosts = () => {
+  const promise = axios.get('https://jsonplaceholder.typicode.com/posts');
 
   return {
     type: 'FETCH_POSTS',
-    payload: response.data,
+    payload: promise,
   };
 };
