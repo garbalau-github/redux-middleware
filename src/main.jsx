100a1128e0297fa9d69ade6c1ee91c06054afdc5
Nick Garbalau <garbalaunick@protonmail.com>
2022-07-17 19:24:17 +0300

    Add multiple middlewares, modify Action Object before getting to Reducer

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,39 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { countReducer } from './state/countReducer';
 
-const myLogger = (store) => (next) => (action) => {
-  console.log(store.getState());
-  console.log(action);
-  console.log('-- Middleware');
+// 1
+const firstMiddleware = (store) => (next) => (action) => {
+  console.log('firstMiddleware()');
   return next(action);
 };
 
-const store = createStore(countReducer, applyMiddleware(myLogger));
+// 2
+const secondMiddleware = (store) => (next) => (action) => {
+  console.log('secondMiddleware()');
+  return next(action);
+};
+
+// 3
+const changeIncrementingBehaviour = (store) => (next) => (action) => {
+  console.log('changeIncrementingBehaviour');
+  console.log('---------------------------');
+
+  // Change behaviour
+  if (store.getState() >= 10) {
+    // I can modify Action properties, or send updated Object
+    return next({ type: 'DECREMENT' });
+  }
+  return next(action);
+};
+
+const store = createStore(
+  countReducer,
+  applyMiddleware(
+    firstMiddleware,
+    secondMiddleware,
+    changeIncrementingBehaviour
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
